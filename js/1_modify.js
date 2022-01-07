9cdad1fab67c026ce1cb4fc3754821131950cd4c
RonakGiri A Goswami <82793358+imRonakGiri@users.noreply.github.com>
2022-01-07 15:52:14 +0530

    Update 1_modify.js

--- js/1_modify.js
+++ js/1_modify.js
@@ -1,3 +1,4 @@
-$(
-  ".hero .title"
-).innerHTML = `We Create Great Web-Solution </br> For Your Business`;
+// $(
+//   ".hero .title"
+// ).innerHTML = `We Create Great Web-Solution </br> For Your Business`;
+import * as a from 'http://localhost:5001/fire-func-lover/us-central1/helloWorld'
